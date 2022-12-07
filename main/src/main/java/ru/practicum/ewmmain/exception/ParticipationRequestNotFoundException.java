419a5f8fbb6f397bf40af6d0055a0e6d4e0e81b3
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-12-07 18:22:42 +0300

    fix: проверить работу в контейнерах, удалить папки для тестов, вернуть оригинальные настройки application.properties, убрать DROP TABLE из схем БД.

--- main/src/main/java/ru/practicum/ewmmain/exception/ParticipationRequestNotFoundException.java
+++ main/src/main/java/ru/practicum/ewmmain/exception/ParticipationRequestNotFoundException.java
@@ -2,6 +2,6 @@ package ru.practicum.ewmmain.exception;
 
 public class ParticipationRequestNotFoundException extends RuntimeException {
     public ParticipationRequestNotFoundException(long id) {
-        super(String.format("Запрос с id = %d не найдена!", id));
+        super(String.format("Заявка с id = %d не найдена!", id));
     }
 }
